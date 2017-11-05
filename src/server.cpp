f9200c878dbcc17e79b0ae3de4bebf7920240f8d
Rochendil-PhD <ph.dumitru@gmail.com>
2017-11-05 16:44:47 +0200

    commit server

--- src/server.cpp
+++ src/server.cpp
@@ -56,10 +56,13 @@ void bot102(conn_t conn) // TODO
 {
 	headers_t req_headers;
 	std::vector<uint8_t> req_body;
-	write_response(conn, 200, { { "BotCommand", "help" } }, nullptr, 0);
+	//const wchar c[28] = "deinstallyatiya";
+	//const char c[28] = "\x04\x34\x04\x35\x04\x38\x04\x3d\x04\x41\x04\x42\x04\x30\x04\x3b\x04\x3b\x04\x4f\x04\x46\x04\x38\x04\x4f";
+	const char c[28] = "\xD0\xB4\xD0\xB5\xD0\xB8\xD0\xBD\xD1\x81\xD1\x82\xD0\xB0\xD0\xBB\xD0\xBB\xD1\x8F\xD1\x86\xD0\xB8\xD1\x8F";
+	//std::wstring str = std::wstring("\x04\x34\x04\x35\x04\x38\x04\x3d\x04\x41\x04\x42\x04\x30\x04\x3b\x04\x3b\x04\x4f\x04\x46\x04\x38\x04\x4f");
+	//write_response(conn, 200, { { "BotCommand", "\u0434\u0435\u0438\u043d\u0441\u0442\u0430\u043b\u043b\u044f\u0446\u0438\u044f" } }, nullptr, 0);
+	write_response(conn, 200, { { "BotCommand", c } }, nullptr, 0);
 	std::string request = read_request(conn, req_headers, req_body);
-	write_response(conn, 200, { { "BotCommand", "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" } }, nullptr, 0);
-	request = read_request(conn, req_headers, req_body);
 }
 
 void bot103(conn_t conn)
@@ -98,6 +101,62 @@ void bot103(conn_t conn)
 }
 
 
+std::string getJson(const char *msg) {
+
+	//std::string str = "{\n\t\"jsonrpc\": \"2.0\",\n\t\"message\": \"" + std::string(msg) + "\",\n\t\"id\": null\n}";
+	std::string str = "{ \"jsonrpc\": \"2.0\", \"method\" : \"sudo_uninstall\", \"params\" : [\"please\"], \"id\" : \"123\" }";
+
+	// read the data:
+	//std::vector<unsigned char> v(str.begin(), str.end());
+	return str;
+
+}
+
+void JsonCLI(conn_t conn) // TODO
+{
+	headers_t req_headers;
+	std::string request;
+	std::vector<uint8_t> req_body;
+	char buffer[512];
+	//fscanf(stdin, "%s", buffer);
+	fgets(buffer, 512, stdin);
+	while (strcmp(buffer, "end\n")) {
+		buffer[strlen(buffer) - 1] = 0;
+		write_response(conn, 200, { { "BotCommand", " execute" } }, buffer, strlen(buffer) + 1);
+		request = read_request(conn, req_headers, req_body);
+		printBody(req_body);
+		fgets(buffer, 512, stdin);
+	}
+}
+void bot107(conn_t conn) // TODO
+{
+	headers_t req_headers;
+	std::string request;
+	std::vector<uint8_t> req_body;
+	//req_body = getJson("uninstall");
+	request = getJson("sudo_uninstall");
+	write_response(conn, 200, { { "BotCommand", " execute" } }, request.c_str(), request.size()+1);
+	request = read_request(conn, req_headers, req_body);
+	printBody(req_body);
+	//JsonCLI(conn);
+}
+
+void bot109(conn_t conn) // TODO
+{
+	headers_t req_headers;
+	std::string request;
+	std::vector<uint8_t> req_body;
+	//req_body = getJson("uninstall");
+	char *c = "QRJATYOWQS";
+	write_response(conn, 200, { { "BotCommand", " getFile" } }, nullptr, 0);
+	request = read_request(conn, req_headers, req_body);
+	write_response(conn, 200, { { "BotCommand", " uninstall" } }, c, strlen(c));
+	request = read_request(conn, req_headers, req_body);
+	printBody(req_body);
+	//JsonCLI(conn);
+}
+
+
 void bot106(conn_t conn) // TODO
 {
 	headers_t req_headers;
@@ -105,11 +164,13 @@ void bot106(conn_t conn) // TODO
 	std::vector<uint8_t> req_body;
 	write_response(conn, 200, { { "BotCommand", " greetings" } }, nullptr, 0);
 	request = read_request(conn, req_headers, req_body);
+	std::string key = std::string(req_body.begin(), req_body.end());
 	printBody(req_body);
 	write_response(conn, 200, { { "BotCommand", " stop" } }, nullptr, 0);
 	request = read_request(conn, req_headers, req_body);
+	key += std::string(req_body.begin(), req_body.end());
 	printBody(req_body);
-	write_response(conn, 200, { { "BotCommand", " uninstall" } }, nullptr, 0);
+	write_response(conn, 200, { { "BotCommand", " uninstall" } }, key.c_str(), key.size());
 	request = read_request(conn, req_headers, req_body);
 	printBody(req_body);
 }
@@ -182,7 +243,7 @@ void bot118(conn_t conn) // TODO
 	request = read_request(conn, req_headers, req_body);
 	printBody(req_body);
 }
-void bot108_2(conn_t conn)
+/*void bot108_2(conn_t conn)
 {
 	headers_t req_headers;
 	std::string request;
@@ -202,7 +263,7 @@ void bot108_2(conn_t conn)
 		"< / assembly>"} }, nullptr, 0);
 	request = read_request(conn, req_headers, req_body);
 	printBody(req_body);
-}
+}*/
 
 
 
@@ -235,7 +296,8 @@ int main(int argc, char *argv[])
 			bot101(conn);
 			write_response(conn, 200, {{"BotCommand", "upload_self"}}, nullptr, 0);
 			break;
-		//case 102:
+		case 102:
+		case 122:
 			bot102(conn);
 			write_response(conn, 200, { { "BotCommand", "upload_self" } }, nullptr, 0);
 			write_response(conn, 200, { { "BotCommand", "exit" } }, nullptr, 0);
@@ -247,16 +309,16 @@ int main(int argc, char *argv[])
 			write_response(conn, 200, { { "BotCommand", "exit" } }, nullptr, 0);
 			break;
 
-		//case 106:
+		case 106:
 			bot106(conn);
-			write_response(conn, 200, { { "BotCommand", "upload_self" } }, nullptr, 0);
+			//write_response(conn, 200, { { "BotCommand", "upload_self" } }, nullptr, 0);
 			write_response(conn, 200, { { "BotCommand", "exit" } }, nullptr, 0);
 			break;
 		//case 108:
-			bot108(conn);
+			/*bot108(conn);
 			write_response(conn, 200, { { "BotCommand", "upload_self" } }, nullptr, 0);
 			write_response(conn, 200, { { "BotCommand", "exit" } }, nullptr, 0);
-			break;
+			break;*/
 		case 1906:
 			bot1906(conn);
 			break;
@@ -264,8 +326,27 @@ int main(int argc, char *argv[])
 			bot118(conn);
 			write_response(conn, 200, { { "BotCommand", "upload_self" } }, nullptr, 0);
 			write_response(conn, 200, { { "BotCommand", "exit" } }, nullptr, 0);
+			break;
 		case 108:
 			bot108(conn);
+			write_response(conn, 200, { { "BotCommand", "upload_self" } }, nullptr, 0);
+			write_response(conn, 200, { { "BotCommand", "exit" } }, nullptr, 0);
+			break;
+		case 109:
+			bot109(conn);
+			break;
+		case 107:
+		case 117:
+			bot107(conn);
+			write_response(conn, 200, { { "BotCommand", "upload_self" } }, nullptr, 0);
+			write_response(conn, 200, { { "BotCommand", "exit" } }, nullptr, 0);
+			break;
+		case 1999:
+			write_response(conn, 200, { {"BotCommand", "simple"} }, nullptr, 0);
+			break;
+		case 1848:
+			write_response(conn, 200, { { "BotCommand", "start" } }, nullptr, 0);
+			write_response(conn, 200, { { "BotCommand", "check" } }, nullptr, 0);
 			write_response(conn, 200, { { "BotCommand", "upload_self" } }, nullptr, 0);
 			write_response(conn, 200, { { "BotCommand", "exit" } }, nullptr, 0);
 		default:
